c07ff713b633b79ff20c1f31bafa44fe5b5cb5fd
rafael-aparicio <62367920+rafael-aparicio@users.noreply.github.com>
2020-03-21 17:44:50 -0300

    Classe Profutos alterada

--- LojaRoupas/LojaRoupas/Classes/Produto.cs
+++ LojaRoupas/LojaRoupas/Classes/Produto.cs
@@ -6,16 +6,89 @@ using System.Threading.Tasks;
 
 namespace LojaRoupas.Classes
 {
-    class Produto
+    public class Produto
+
     {
-        /*- ID int
-        - CodigoBarras
-        - descrição
-        - cor
-        - tamanho
-        - precoCusto
-        - precoVenda
-        - qtdEstoque
-        */
+
+        private string nome_produto;
+
+        private string fornecedor_produto;
+
+        private string fabricante_produto;
+
+        private string obs_produto;
+
+        private double preco_produto;
+
+        private double quantidade_produto;
+
+
+
+        public string Nome_produto
+
+        {
+
+            get { return nome_produto; }
+
+            set { nome_produto = value; }
+
+
+
+        }
+
+
+
+        public string Fornecedor_produto
+
+        {
+
+            get { return fornecedor_produto; }
+
+            set { fornecedor_produto = value; }
+
+
+
+        }
+
+        public string Fabricante_produto
+
+        {
+            get { return fabricante_produto; }
+
+            set { fabricante_produto = value; }
+        }
+
+        public string Obs_produto
+
+        {
+
+            get { return obs_produto; }
+
+            set { obs_produto = value; }
+
+        }
+
+        public double Preco_produto
+
+        {
+
+            get { return preco_produto; }
+
+            set { preco_produto = value; }
+        }
+
+        public double Quantidade_produto
+
+        {
+
+            get { return quantidade_produto; }
+
+            set { quantidade_produto = value; }
+
+
+
+        }
+
     }
+
 }
